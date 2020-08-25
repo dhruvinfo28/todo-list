9dd29e68f3757972e4ea64920548a6a87642a470
dhruvinfo28 <dhruv.info28@gmail.com>
2020-08-26 02:23:04 +0530

    Added a database using mongoose to this app

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs');
+const mongoose = require('mongoose')
 const date = require(__dirname+'/date.js')
 app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');  // From ejs documentation
 app.use(express.static("public"));
-var newItems = [];
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true, useUnifiedTopology: true})
+const itemSchema = mongoose.Schema({
+    name: String
+})
+const Item = mongoose.model("Item",itemSchema);
+const cookFood = new Item({
+    name:"Use the + button to add new item"
+})
+const eatFood = new Item({
+    name:"<-- Use this to delete"
+})
+// Item.insertMany([cookFood,eatFood],function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Successfully added two items");
+//     }
+// })
 app.get("/",(req,res)=>{
     var day = date.getDate(); //Code refactored here
-    res.render("list",{dayName:day,newListItems:newItems})
-
+    Item.find(function(err,data){
+        if(err){
+            console.log("read error: "+ err)
+        }else{
+            res.render("list",{dayName:day,newListItems:data})
+          }
+    })
 })
 app.post("/",(req,res)=>{
     var newItem = req.body.newItem
     if(newItem!=""){
-     newItems.push(newItem)
+      var newItem_add = new Item({
+          name:newItem
+      })
+      newItem_add.save();
     }
     //Now we cant use res.render("list",{newListItem:newItem})
     //becasue when somebody makes a get request to our homepage we initially land in the codeblock above, and we render only dayName value, so the browser wont have a value of newListItem at that time , so now if we render it on the get request codeblock then we wont have the value of newItem, so we make newItem a global variable and set its value in the post request
@@ -23,7 +50,14 @@ app.post("/",(req,res)=>{
 })
 app.post("/remove",(req,res)=>{
     let remove = req.body.remove;
-    newItems.splice(newItems.indexOf(remove),1) ;
+    console.log(remove);
+    Item.deleteOne({name:remove},function(err){
+        if(err){
+            console.log("Delete error :" + err)
+        }else{
+            console.log("Delete Successful")
+        }
+    })
     res.redirect("/") 
 })
 app.listen(3000,()=>{
